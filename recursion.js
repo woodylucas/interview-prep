db418a28063ea4412b51a35fc63a10f839b19c62
woodylucas <wlucas0311@gmail.com>
2020-07-24 16:48:54 -0400

    solution one

--- recursion.js
+++ recursion.js
@@ -1,3 +1,22 @@
+// Challenge 1
+function countdown(n, newArr = []) {
+    // BASE CASE: 
+    if(n <= 0) return newArr // IF num is zero RETURN newArr 
+    
+    newArr.push(n)// PUSH elements into newArr
+    
+    // RECURSIVE CALL: 
+    return countdown(n - 1, newArr) // RETURN countdown invoking parameters n - 1, newArr
+  }
+  
+  // To check if you've completed it, uncomment these console.logs!
+  // console.log(countdown(5));
+  // console.log(countdown(10));
+  
+
+
+
+
 //Challenge 6
 function flattenRecursively(arr) {
     // BASE: case: 
