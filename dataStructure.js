219071295ea099bda165be259d62ff7938d85cb7
woodylucas <wlucas0311@gmail.com>
2020-08-05 22:03:36 -0400

    implemented my first data structure--queue

---
+++ dataStructure.js
@@ -0,0 +1,14 @@
+// Queues
+
+class Queue {
+    constructor(){
+        this.data = [];
+    }
+    add(record) {
+        this.data.unshift(record)
+    }
+
+    remove() {
+        return this.data.pop()
+    }
+}
