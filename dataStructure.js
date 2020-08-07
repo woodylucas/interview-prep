e3840bf2836722e0b9a5f9823830038cc755656c
woodylucas <wlucas0311@gmail.com>
2020-08-06 21:33:59 -0400

    basic foundations of stack data structure

--- dataStructure.js
+++ dataStructure.js
@@ -1,8 +1,8 @@
-// Queues
+// Queues from scatch.
 
 class Queue {
     constructor(){
-        this.data = [];
+        this.data = []; a 
     }
     add(record) {
         this.data.unshift(record)
@@ -12,3 +12,33 @@ class Queue {
         return this.data.pop()
     }
 }
+
+// STACK
+
+// --- Directions
+// Create a stack data structure.  The stack
+// should be a class with methods 'push', 'pop', and
+// 'peek'.  Adding an element to the stack should
+// store it until it is removed.
+// --- Examples
+//   const s = new Stack();
+//   s.push(1);
+//   s.push(2);
+//   s.pop(); // returns 2
+//   s.pop(); // returns 1
+
+class Stack {
+    constructor() {
+        this.data = [];
+    }
+    push(record) {
+        this.data.push(record);
+    }
+    pop(){
+        return this.data.pop();
+    }
+    peek(){
+        return this.data[this.data.length - 1];
+    }
+}
+
