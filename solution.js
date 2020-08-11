c30c3cc9707a999ae205e8d3be39df11dd4816dd
woodylucas <wlucas0311@gmail.com>
2020-08-11 14:39:06 -0400

    solved middle link node solution

--- solution.js
+++ solution.js
@@ -1,4 +1,5 @@
 // Leet Code linked list solutons
+
 // CYCLE 
 var hasCycle = function(head) {
     let slow = head; // Declare a slow variable set to head 
@@ -13,4 +14,17 @@ var hasCycle = function(head) {
             
     }
     return false;// RETURN false
+};
+
+// Middle Link node --> second node element if its even 
+
+var middleNode = function(head) {
+    let slow = head; // Declare a variable slow set to head; 
+    let fast = head; // Declare a variable fast set to head 
+    
+    while (fast && fast.next) { // While fast isn't null AND a property of next isn't null 
+        slow = slow.next;  // Reassign the slow property to next 
+        fast = fast.next.next; // Reassign fast property to the next property after next until condition is met
+    }
+    return slow; // RETURN slow.
 };
