67ad058d060c1f075e9169574e42b507f529cad8
woodylucas <wlucas0311@gmail.com>
2020-09-04 14:52:47 -0400

    merge sort practice

--- solution.js
+++ solution.js
@@ -579,3 +579,52 @@ function characterMode(string) {
  }
   return maxCharacters.join('');// RETURN maxcharacters
 }
+
+
+function letterSort(string) {
+  // YOUR WORK HERE 
+  const acsii = acsiiArray(string); 
+  function mergeSort(arr) {
+    if (arr.length <= 1) return arr; 
+    let midpoint = Math.floor(arr.length / 2); 
+    let left = mergeSort(arr.slice(0, midpoint)); 
+    let right = mergeSort(arr.slice(midpoint)); 
+    return merge(left, right); 
+  }
+  return String.fromCharCode(...mergeSort(acsii)); 
+}
+// Helper methods
+
+// merge function 2 arrays 
+function merge(arr1, arr2) {
+  let i = 0; 
+  let j = 0;
+  const results = [];
+  while (i < arr1.length && j < arr2.length) {
+    if (arr2[j] > arr1[i]) {
+      results.push(arr1[i]);
+      i++; 
+    } else {
+      results.push(arr2[j]);
+      j++; 
+    }
+  }
+  while (i < arr1.length) {
+    results.push(arr1[i]); 
+    i++;  
+  }
+  while (j < arr2.length) {
+    results.push(arr2[j]);
+    j++; 
+  }
+  return results;
+}
+// acsii array conveter 
+function acsiiArray(string) {
+  const acsiiArray = [];
+  for (const letter of string) {
+    const acsii = letter.charCodeAt(); 
+    acsiiArray.push(acsii);
+  }
+  return acsiiArray
+}
