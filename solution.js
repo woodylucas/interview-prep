3df7b08a39bf242fd6c02cc848d4afcd29de4fca
woodylucas <wlucas0311@gmail.com>
2020-09-10 16:48:19 -0400

    is bad version--solution

--- solution.js
+++ solution.js
@@ -861,3 +861,21 @@ var search = function(nums, target) {
   }
   return - 1; 
 };
+
+
+var solution = function(isBadVersion) {
+
+  return function(n) {
+      let left = 1;
+      let right = n; 
+      while (left < right) {
+          let middle = left + Math.floor((right - left) / 2);
+          if (!isBadVersion(middle)) {
+              left = middle + 1; 
+          } else {
+              right = middle;
+          }
+      }
+      return left;
+  };
+};
