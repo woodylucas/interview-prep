a736084fb559f6df772d8bb43a582a40c5fb5896
woodylucas <wlucas0311@gmail.com>
2020-09-14 10:01:45 -0400

    dynamic programming practice

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -34,4 +34,21 @@ function longestPalindromicSubstring(string) {
       } 
       return [leftIdx + 1, rightIdx]; 
   }
+
+
+  function maxSubsetSumNoAdjacent(array) {
+    // Write your code here.
+      // O(n) time | O(n) space
+      if (!array.length) return 0; 
+      if (array.length === 1) return array[0];
+      let second = array[0]; 
+      let first = Math.max(array[0], array[1]); 
+      for (let i = 2; i < array.length; i++) {
+          const current = Math.max(first, second + array[i]); 
+          second = first; 
+          first = current; 
+      }
+      return first; 
+  }
+  
   
