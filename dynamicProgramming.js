f3fb5298248cd74d076c6f03bd17e8682e4ceaff
woodylucas <wlucas0311@gmail.com>
2020-09-20 11:23:33 -0400

    solved trapping rain--very intuitive once you understand the problem--more optimal approach after refactor

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -241,3 +241,27 @@ function maxProduct(nums) {
     }
     return max;   
 };
+
+
+function waterArea(heights) {
+    // Write your code here.
+      const maxes = Array.from({length: heights.length }, () => 0); 
+      let leftMax = 0; 
+      for (let i = 0; i < heights.length; i++) {
+          const height = heights[i]; 
+          maxes[i] = leftMax; 
+          leftMax = Math.max(leftMax, height)
+      }
+      let rightMax = 0; 
+      for (let i = heights.length - 1; i >= 0; i--) {
+          const height = heights[i]; 
+          const minHeight = Math.min(rightMax, maxes[i]); 
+          if (height < minHeight) {
+              maxes[i] = minHeight - height;
+          } else {
+              maxes[i] = 0; 
+          }
+          rightMax = Math.max(rightMax, height); 
+      }
+      return maxes.reduce((a, b) => a + b, 0); 
+  }
