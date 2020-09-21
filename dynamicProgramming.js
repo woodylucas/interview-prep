6f9273eb7c8c04369ca2c50144cb84352aba3805
woodylucas <wlucas0311@gmail.com>
2020-09-21 10:33:38 -0400

    solved longestPalindromicSubstring--tabulaton

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -308,4 +308,26 @@ function waterArea(heights) {
       }
       return string.slice(longest[0], longest[1]); 
   }
+
+  function longestPalindromicSubstring(string) {
+    // Write your code here.
+      let currentLongest = [0, 1]; // initialize an array starting at 0 and 1 --> i + 1
+      for (let i = 1; i < string.length; i++) {
+          const odd = expandFromMiddle(string, i - 1, i); // a b a
+          const even  = expandFromMiddle(string, i - 1, i + 1); // ab ba
+          const longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even; 
+          currentLongest = currentLongest[1] - currentLongest[0] > longest[1] - longest[0] ? currentLongest : longest
+      }
+      return string.slice(currentLongest[0], currentLongest[1]); 
+  }
+  
+  
+  function expandFromMiddle(string, leftIdx, rightIdx) {
+      while (leftIdx >= 0 && rightIdx < string.length) {
+          if (string[leftIdx] !== string[rightIdx]) break; 
+          leftIdx--; 
+          rightIdx++; 
+      } 
+      return [leftIdx + 1, rightIdx]; 
+  }
   
