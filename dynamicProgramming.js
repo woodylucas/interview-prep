4ac6dc9e4594fcf1f7fc1251638144ef0159849b
woodylucas <wlucas0311@gmail.com>
2020-09-22 18:09:39 -0400

    knapsack solution

--- dynamicProgramming.js
+++ dynamicProgramming.js
@@ -331,3 +331,59 @@ function waterArea(heights) {
       return [leftIdx + 1, rightIdx]; 
   }
   
+
+  function getNthFib(n) {
+    // Write your code here.
+      const lastTwo = [0, 1]; 
+      let counter = 3; 
+      while (counter <= n) {
+          const nextFib = lastTwo[0] + lastTwo[1]; 
+          lastTwo[0] = lastTwo[1]
+          lastTwo[1] = nextFib; 
+          counter++; 
+      }
+      return n <= 1 ?  lastTwo[0] : lastTwo[1]; 
+  }
+
+  function knapsackProblem(items, capacity) {
+    // Write your code here.
+      // Build a subarray filled with zeros.
+   const knapsackValues = []; 
+      for (let i = 0; i < items.length + 1; i++) {
+          const row = Array.from({length: capacity + 1}, () => 0); 
+          knapsackValues.push(row); 
+      }
+      for (let i = 1; i < items.length + 1; i++) {
+          const currentWeight = items[i - 1][1]; 
+          const currentValue = items[i - 1][0]; 
+          for (let c = 0; c < capacity + 1; c++) {
+              if (currentWeight > c) {
+                  knapsackValues[i][c] = knapsackValues[i - 1][c]; 
+              } else {
+                  knapsackValues[i][c] = Math.max(
+                      knapsackValues[i - 1][c], 
+                      knapsackValues[i - 1][c - currentWeight] + currentValue,
+                  ); 
+              }
+          }
+      }
+      return [knapsackValues[items.length][capacity], getKnapsackItems(knapsackValues, items)]; 
+  }
+  
+  function getKnapsackItems(knapsackValues, items) {
+      const sequence = []; 
+      let i = knapsackValues.length - 1; 
+      let c = knapsackValues[0].length - 1; 
+      while (i > 0) {
+          if (knapsackValues[i][c] === knapsackValues[i - 1][c]) {
+              i -= 1; 
+          } else {
+              sequence.unshift(i - 1); 
+              c -= items[i - 1][1]; 
+              i -= 1; 
+          }
+          if (c === 0) break;
+      }
+      return sequence; 
+  }
+  
