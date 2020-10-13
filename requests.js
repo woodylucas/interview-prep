57a4523d9dbca3714e40ea8ce216c1d74f1bf3db
woodylucas <wlucas0311@gmail.com>
2020-10-13 09:46:57 -0400

    XMLHTTPRequest just trying to obtain a more conceptual overiew on using requests

---
+++ requests.js
@@ -0,0 +1,18 @@
+// XMLHttpRequest
+
+const firstRequest = new XMLHttpRequest(); 
+firstRequest.addEventListener('load', function () {
+    console.log('IT WORKED'); 
+    const data = JSON.parse(this.responseText);
+    for (const planet of data.results) {
+        console.log(planet.name);
+    }
+    console.log(data)
+;})
+
+firstRequest.addEventListener('error', () => {
+    console.log('IT WORKED'); 
+})
+firstRequest.open('GET', "https://swapi.dev/api/planets/"); 
+firstRequest.send(); 
+console.log('Request Sent')
